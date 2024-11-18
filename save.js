083ba1365d350e937d456a5d775db0755a8470d0
bbartika <137605070+bbartika@users.noreply.github.com>
2024-11-18 04:16:48 -0800

    Add files via upload

--- save.js
+++ save.js
@@ -1,24 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
-const Details = require('../models/details');
+const saveController = require('../controller/save');
 
+router.post('/save-details', saveController.saveDetails);
 
-exports.saveDetails = async(req,res)=>{
-    console.log("request is coming", req.body)
-    try{
-        const {Name, Email, Contactnumber} = req.body;
-
-        await Details.create({
-            name: Name,
-            email: Email,
-            contactnumber: Contactnumber
-        });
-        res.status(200).json("Details saved successfully");
-
-        
-
-    }catch(err){
-console.log(err)
-        res.status(500).json({error: err});
-
-    }
-}
+module.exports = router;
